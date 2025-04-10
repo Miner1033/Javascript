f7da299e5ad14ddb8c36988a8f14eeb1a1a1f939
Md.Miner Hossain Rimon <minerhossainrimon1033@gmail.com>
2025-04-10 23:54:20 +0600

    Add files via upload

---
+++ variable1.js
@@ -0,0 +1,25 @@
+console.log("miner");
+
+// 🔹 What is a Variable in JavaScript?
+// A variable is like a container used to store data (like numbers, strings, etc.) that you can use and change later in your program.
+
+// Think of it like a box where you store a value. You give the box a name, so you can find and use the value later.
+
+// 🔹 How to Declare a Variable?
+// You can declare a variable using these keywords:
+
+// Keyword	Description
+// var	Old way of declaring variables. Has function scope.
+// let	Modern way. Has block scope (recommended).
+// const	Used for values that don't change.
+let name="rimon";
+const pi=3.14;
+var age=20;
+console.log(name);
+    console.log(pi);
+console.log(age);
+// Output: rimon
+let n=20;
+let m=30;
+let sum=n+m;
+console.log(sum);
