91abf20dceea96bedd28d8c832ec99e592de0802
Md.Miner Hossain Rimon <minerhossainrimon1033@gmail.com>
2025-04-11 00:49:01 +0600

    Add files via upload

---
+++ 3rd.js
@@ -0,0 +1,112 @@
+// 🔹 1. What is a Function?
+// A function is a block of code designed to perform a specific task. You can reuse it anywhere in your code.
+
+// function greet() {
+//     console.log("Hello, world!");
+//   }
+//   greet(); // Output: Hello, world!
+
+// 2. How to Declare JavaScript Function?
+// function functionName(parameters) {
+//     // code to run
+//   }
+
+// function add(a, b) {
+//     return a + b;
+//   }
+//   console.log(add(3, 5)); // Output: 8
+
+// 🔹 3. JavaScript Data Types
+// JavaScript has 8 basic data types:
+
+// Type	Example
+// String	"Hello"
+// Number	123, 3.14
+// Boolean	true, false
+// Undefined	let x;
+// Null	let x = null;
+// Object	{name: "Rimon"}
+// Array	[1, 2, 3]
+// Symbol	Symbol('id')
+
+// 🔹 4. JavaScript Operators
+// Operators perform actions on variables and values.
+
+// ✅ Types of operators:
+// Arithmetic: +, -, *, /, %
+
+// Assignment: =, +=, -=, *=, /=
+
+// Comparison: ==, ===, !=, >, <
+
+// Logical: &&, ||, !
+// let a = 10;
+// let b = 5;
+// console.log(a + b); // Output: 15
+/*
+ This is a 
+ multi-line comment
+*/
+// let x = 5;
+// x = x + 2;
+// console.log(x); // 7
+// 🔹 7. JavaScript Event List
+// Events are actions like click, hover, load, etc.
+
+// Event	Description
+// onclick	User clicks an element
+// onload	Page finishes loading
+// onmouseover	Mouse over an element
+// onkeydown	Key is pressed
+// onchange	Value of input changes
+// let age = 20;
+
+// if (age < 18) {
+//   console.log("Minor");
+// } else if (age == 18) {
+//   console.log("Just adult");
+// } else {
+//   console.log("Adult");
+// }
+// let day = 2;
+// switch(day) {
+//   case 1: console.log("Monday"); break;
+//   case 2: console.log("Tuesday"); break;
+//   default: console.log("Other day");
+// }
+
+// for(let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+  
+
+// let i = 0;
+// while(i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while(i < 5);
+
+
+// let fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits[0]); // Output: Apple
+  
+// 11. Error Handling
+try {
+    let result = 10 / 0;
+    console.log(result);
+  } catch (error) {
+    console.log("Something went wrong!");
+  }
+  
+
+
+
+
+  
+  
---
+++ input.js
@@ -0,0 +1,5 @@
+// console.log()	Shows output in the browser’s console (for developers)
+// alert()	Shows a popup message on the screen
+// document.write()	Writes output directly to the web page
+// innerHTML	Puts output inside a specific HTML element
+alert("Hello, World!"); // Displays a popup message with "Hello, World!"
